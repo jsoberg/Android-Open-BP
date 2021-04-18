aa137af84cee84a5a1c465efe8301c2e21992232
Joshua Soberg <j15soberg@gmail.com>
2021-04-18 11:13:34 -0400

    Add initial colors and theme for app

---
+++ app/src/main/kotlin/com/soberg/openbp/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.soberg.openbp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Cyan200 = Color(0xFF80DEEA)
+val Cyan300 = Color(0xFF4DD0E1)
+val Cyan700 = Color(0xFF0097A7)
+val Cyan800 = Color(0xFF00838F)
+val Cyan900 = Color(0xFF006064)
