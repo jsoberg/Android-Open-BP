be9c10b3ac18b78748c84aab2190410799664aef
Joshua Soberg <j15soberg@gmail.com>
2021-07-05 21:27:18 -0400

    Remove compose dependencies

--- buildSrc/src/main/java/com/soberg/openbp/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/com/soberg/openbp/buildsrc/dependencies.kt
@@ -5,7 +5,7 @@ object Deps {
     const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0-alpha15"
 
     const val junit = "junit:junit:4.13"
-    const val material = "com.google.android.material:material:1.1.0"
+    const val material = "com.google.android.material:material:1.3.0"
 
     object Sdk {
         object Version {
@@ -33,24 +33,9 @@ object Deps {
         const val appcompat = "androidx.appcompat:appcompat:1.3.0-beta01"
         const val coreKtx = "androidx.core:core-ktx:1.5.0-beta03"
 
-        object Activity {
-            const val activityCompose = "androidx.activity:activity-compose:1.3.0-alpha05"
-        }
-
-        object Compose {
-            const val version = "1.0.0-beta03"
-            const val foundation = "androidx.compose.foundation:foundation:$version"
-            const val layout = "androidx.compose.foundation:foundation-layout:$version"
-            const val material = "androidx.compose.material:material:$version"
-            const val materialIconsExtended =
-                "androidx.compose.material:material-icons-extended:$version"
-            const val runtime = "androidx.compose.runtime:runtime:$version"
-            const val runtimeLivedata = "androidx.compose.runtime:runtime-livedata:$version"
-            const val tooling = "androidx.compose.ui:ui-tooling:$version"
-            const val test = "androidx.compose.ui:ui-test:$version"
-            const val uiTest = "androidx.compose.ui:ui-test-junit4:$version"
-            const val uiUtil = "androidx.compose.ui:ui-util:${version}"
-            const val viewBinding = "androidx.compose.ui:ui-viewbinding:$version"
+        object ConstraintLayout {
+            private const val version = "2.0.4"
+            const val constraintLayout = "androidx.constraintlayout:constraintlayout:$version"
         }
 
         object Lifecycle {
@@ -58,8 +43,6 @@ object Deps {
             const val extensions = "androidx.lifecycle:lifecycle-extensions:$version"
             const val livedata = "androidx.lifecycle:lifecycle-livedata-ktx:$version"
             const val viewmodel = "androidx.lifecycle:lifecycle-viewmodel-ktx:$version"
-            const val viewModelCompose =
-                "androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha03"
         }
 
         object Navigation {
