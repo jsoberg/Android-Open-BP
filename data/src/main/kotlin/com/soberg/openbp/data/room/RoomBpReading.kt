37c3759c73f8e2ba9d08f2fe648898609a3897c9
Joshua Soberg <j15soberg@gmail.com>
2021-08-01 15:42:10 -0400

    Add a method for converting from db model to data model, and mark ID as auto-in

--- data/src/main/kotlin/com/soberg/openbp/data/room/RoomBpReading.kt
+++ data/src/main/kotlin/com/soberg/openbp/data/room/RoomBpReading.kt
@@ -3,11 +3,14 @@ package com.soberg.openbp.data.room
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.soberg.openbp.data.BpReading
+import com.soberg.openbp.data.bpm
+import com.soberg.openbp.data.mmHg
 import com.soberg.openbp.data.room.RoomBpReading.Companion.TABLE_NAME
 
 @Entity(tableName = TABLE_NAME)
 data class RoomBpReading(
-    @PrimaryKey val id: Long,
+    @PrimaryKey(autoGenerate = true) val id: Long,
     @ColumnInfo(name = SYSTOLIC) val systolic: Short,
     @ColumnInfo(name = DIASTOLIC) val diastolic: Short,
     @ColumnInfo(name = RECORDED_TIME) val recordedTime: Long,
@@ -22,4 +25,12 @@ data class RoomBpReading(
         const val RECORDED_TIME = "recorded_time_ms"
         const val PULSE = "pulse_bpm"
     }
+
+    fun toDataModel() = BpReading(
+        id = id,
+        systolic = systolic.mmHg,
+        diastolic = diastolic.mmHg,
+        recordedTime = recordedTime,
+        pulse = pulse?.bpm,
+    )
 }
