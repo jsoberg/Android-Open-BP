d14059bb44bb6d4a2a0777e1e9ac99039c998545
Joshua Soberg <j15soberg@gmail.com>
2021-08-01 15:41:45 -0400

    Add a method for instantiating a database singleton

--- data/src/main/kotlin/com/soberg/openbp/data/room/OpenBpRoomDatabase.kt
+++ data/src/main/kotlin/com/soberg/openbp/data/room/OpenBpRoomDatabase.kt
@@ -1,9 +1,31 @@
 package com.soberg.openbp.data.room
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [RoomBpReading::class], version = 1)
 abstract class OpenBpRoomDatabase : RoomDatabase() {
     abstract fun bpReadingDao(): RoomBpReadingDao
+
+    companion object {
+
+        private val DB_LOCK = Any()
+        private var INSTANCE: OpenBpRoomDatabase? = null
+
+        fun get(context: Context): OpenBpRoomDatabase =
+            synchronized(DB_LOCK) {
+                INSTANCE ?: instantiate(context)
+            }
+
+        private fun instantiate(context: Context): OpenBpRoomDatabase {
+            INSTANCE = Room.databaseBuilder(
+                context.applicationContext,
+                OpenBpRoomDatabase::class.java,
+                "open_bp_database"
+            ).build()
+            return INSTANCE!!
+        }
+    }
 }
