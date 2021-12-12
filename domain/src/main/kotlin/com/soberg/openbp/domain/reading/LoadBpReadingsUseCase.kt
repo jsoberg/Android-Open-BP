43c9285c741103c6ac4a96b453d04d14e6e66234
Joshua Soberg <j15soberg@gmail.com>
2021-12-12 13:09:23 -0500

    No reason to create a scope currently for the load readings use case

--- domain/src/main/kotlin/com/soberg/openbp/domain/reading/LoadBpReadingsUseCase.kt
+++ domain/src/main/kotlin/com/soberg/openbp/domain/reading/LoadBpReadingsUseCase.kt
@@ -1,20 +1,10 @@
 package com.soberg.openbp.domain.reading
 
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.CoroutineName
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
-import kotlin.coroutines.CoroutineContext
 
 class LoadBpReadingsUseCase @Inject constructor(
     private val repository: BpReadingRepository,
-    dispatcher: CoroutineDispatcher
 ) {
-    private val useCaseScope = object : CoroutineScope {
-        override val coroutineContext: CoroutineContext =
-            dispatcher + CoroutineName("LoadBpReadingsUseCase.Coroutine")
-    }
-
     fun loadBpReadings(): Flow<List<BpReading>> = repository.getAll()
 }
