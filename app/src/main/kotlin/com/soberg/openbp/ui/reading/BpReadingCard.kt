d7cb6324d9ce60333fd8b703c8fae71ec95b3580
Joshua Soberg <j15soberg@gmail.com>
2021-12-27 21:42:00 -0500

    Add a color indicator for classification on BP reading list items

--- app/src/main/kotlin/com/soberg/openbp/ui/reading/BpReadingCard.kt
+++ app/src/main/kotlin/com/soberg/openbp/ui/reading/BpReadingCard.kt
@@ -1,12 +1,16 @@
 package com.soberg.openbp.ui.reading
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -17,6 +21,7 @@ import androidx.compose.ui.unit.sp
 import com.soberg.openbp.R
 import com.soberg.openbp.domain.classification.Classification
 import com.soberg.openbp.domain.reading.mmHg
+import com.soberg.openbp.ui.theme.*
 
 @Composable
 internal fun BpReadingCard(
@@ -44,30 +49,45 @@ private fun BpReadingCardContent(
     reading: BpReadingViewItem,
 ) {
     Row(
-        modifier = Modifier
-            .padding(
-                start = 8.dp,
-                top = 4.dp
-            )
-            .wrapContentHeight()
+        modifier = Modifier.fillMaxSize()
     ) {
+        val color = colorForClassification(reading.classification)
+        Box(
+            modifier = Modifier
+                .padding(
+                    start = 16.dp,
+                    end = 16.dp
+                )
+                .size(30.dp)
+                .clip(CircleShape)
+                .background(color)
+                .align(Alignment.CenterVertically)
+        )
         Text(
             text = "${reading.systolic}/${reading.diastolic}",
-            modifier = Modifier.align(Alignment.Bottom),
+            modifier = Modifier
+                .align(Alignment.CenterVertically),
             style = PressureTextStyle
         )
         Text(
             text = stringResource(id = R.string.pressure_abbreviation),
             modifier = Modifier
-                .align(Alignment.Bottom)
-                .padding(
-                    start = 4.dp,
-                    bottom = 5.dp
-                ),
+                .align(Alignment.CenterVertically)
+                .padding(start = 4.dp),
             style = PressureAbbrTextStyle
         )
     }
 }
+
+private fun colorForClassification(classification: Classification): Color =
+    when (classification) {
+        // TODO Use themed colors
+        Classification.NORMAL -> Green500
+        Classification.ELEVATED -> Yellow500
+        Classification.HYPERTENSION_STAGE_1 -> Orange500
+        Classification.HYPERTENSION_STAGE_2 -> DeepOrange500
+        Classification.HYPERTENSIVE_CRISIS -> Red500
+    }
 
 private val PressureTextStyle = TextStyle(
     fontFamily = FontFamily.Default,
